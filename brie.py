324810d504247227e41c649b2a2779b17d1dee02
Siddharth Ramesh <siddharth.ramesh93@gmail.com>
2016-02-07 15:23:26 +0530

    updated README

--- brie.py
+++ brie.py
@@ -1,10 +1,13 @@
-# Brie.py v0.9 - Simple data scraper written in Python using BeautifulSoup
-# and Requests.
-# At this version, the scraper works for one site and one show.
+'''Brie.py v0.9 - Simple data scraper written in Python using BeautifulSoup
+ and Requests.
+ At this version, the scraper works for one site and one show.
+'''
 import requests # HTTP for Humans :-)
+import json
 
 from bs4 import BeautifulSoup # Simple but effective scraper library.
 
+data = []
 r = requests.get('https://eztv.ag/search/the-flash') # get page.
 
 soup = BeautifulSoup(r.content, "lxml") # using parser lxml.
@@ -14,9 +17,21 @@ fl_name = soup.find_all("a", {"class": "epinfo"})
 fl_magnet = soup.find_all("a", {"class": "magnet"})
 
 # simple for loops to obtain the data.
+
 for item in fl_name:
     if "720" in item.text:
-        print item.text # print episode name.
+        #print item.text # print episode name.
+        data.append({
+            "title": item.text
+            })
+
 for item in fl_magnet:
     if "720" in item.get("title"):
-        print item.get("href") # print magnet link.
+        #print item.get("href") # print magnet link.
+        data.append({
+            "magnet": item.get("href")
+            })
+
+#print data
+with open("out.json", "w") as outf:
+    json.dump(data, outf, indent=1)
